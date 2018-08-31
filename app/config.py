a46d9ad3f3792f1a44c2dde5149938a47abb636f
Owenmur21 <owenmuriithi@gmail.com>
2018-08-31 14:46:07 +0300

    Adds another api-url

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ class Config:
     '''
     pass
     TOPNEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    CATEGORIES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
 
 
 

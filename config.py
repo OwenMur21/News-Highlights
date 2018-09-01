9292addcf60fdb3f188a837d49da1ac13504507d
Owenmur21 <owenmuriithi@gmail.com>
2018-09-01 13:11:36 +0300

    Moves and updates config file

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    
     TOPNEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     CATEGORIES_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
@@ -28,3 +31,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
